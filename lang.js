44bfb402ca899c15fa840c7bad76fdf71c0c34d1
gigliodou-bit <gigliodou@gmail.com>
2025-10-28 14:38:51 +0100

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,13 @@
+// In futuro potrai aggiungere traduzioni qui
+const translations = {
+  it: {
+    title: "Converti HTML o Siti Web in PDF",
+    button: "Converti in PDF",
+    placeholder: "https://esempio.com o <html>...</html>"
+  },
+  en: {
+    title: "Convert HTML or Websites to PDF",
+    button: "Convert to PDF",
+    placeholder: "https://example.com or <html>...</html>"
+  }
+};
